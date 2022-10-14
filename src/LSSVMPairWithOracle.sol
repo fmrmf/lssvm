09b8005fba15cf4024dccd2cb163cf5d7175b5e1
fmrmf <m@fmrmf.xyz>
2022-10-14 09:59:48 -0400

    init LSSVMPairWithOracle

---
+++ src/LSSVMPairWithOracle.sol
@@ -0,0 +1,230 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ICurve} from "./bonding-curves/ICurve.sol";
+import {LSSVMPair} from "./LSSVMPair.sol";
+import {ILSSVMPairFactoryLike} from "./ILSSVMPairFactoryLike.sol";
+
+/// @title An NFT/Token pair with a price (and liquidity?) oracle
+/// @notice This inherits from the base contract to include an oracle
+/// @dev Optimistically writes observations to oracle based on last swap
+abstract contract LSSVMPairWithOracle is LSSVMPair {
+    // TODO: using Oracle for Oracle.Observation[];
+    // TODO: Orace.Observation[] public observations;
+
+    /**
+     * External state-changing functions
+     */
+
+    /**
+        @notice Sends token to the pair in exchange for any `numNFTs` NFTs
+        @dev To compute the amount of token to send, call bondingCurve.getBuyInfo.
+        This swap function is meant for users who are ID agnostic
+        @dev Overrides base swapTokenForAnyNFTs to optimistically write to oracle prior to calcs and swap.
+        @param numNFTs The number of NFTs to purchase
+        @param maxExpectedTokenInput The maximum acceptable cost from the sender. If the actual
+        amount is greater than this value, the transaction will be reverted.
+        @param nftRecipient The recipient of the NFTs
+        @param isRouter True if calling from LSSVMRouter, false otherwise. Not used for
+        ETH pairs.
+        @param routerCaller If isRouter is true, ERC20 tokens will be transferred from this address. Not used for
+        ETH pairs.
+        @return inputAmount The amount of token used for purchase
+     */
+    function swapTokenForAnyNFTs(
+        uint256 numNFTs,
+        uint256 maxExpectedTokenInput,
+        address nftRecipient,
+        bool isRouter,
+        address routerCaller
+    )
+        external
+        payable
+        virtual
+        override
+        nonReentrant
+        returns (uint256 inputAmount)
+    {
+        // Store locally to remove extra calls
+        ILSSVMPairFactoryLike _factory = factory();
+        ICurve _bondingCurve = bondingCurve();
+        IERC721 _nft = nft();
+
+        // Input validation
+        {
+            PoolType _poolType = poolType();
+            require(
+                _poolType == PoolType.NFT || _poolType == PoolType.TRADE,
+                "Wrong Pool type"
+            );
+            require(
+                (numNFTs > 0) && (numNFTs <= _nft.balanceOf(address(this))),
+                "Ask for > 0 and <= balanceOf NFTs"
+            );
+        }
+
+        // TODO: optimistically write last price to historical observations
+        _writeObservationToOracle();
+
+        // Call bonding curve for pricing information
+        uint256 protocolFee;
+        (protocolFee, inputAmount) = _calculateBuyInfoAndUpdatePoolParams(
+            numNFTs,
+            maxExpectedTokenInput,
+            _bondingCurve,
+            _factory
+        );
+
+        _pullTokenInputAndPayProtocolFee(
+            inputAmount,
+            isRouter,
+            routerCaller,
+            _factory,
+            protocolFee
+        );
+
+        _sendAnyNFTsToRecipient(_nft, nftRecipient, numNFTs);
+
+        _refundTokenToSender(inputAmount);
+
+        emit SwapNFTOutPair();
+    }
+
+    /**
+        @notice Sends token to the pair in exchange for a specific set of NFTs
+        @dev To compute the amount of token to send, call bondingCurve.getBuyInfo
+        This swap is meant for users who want specific IDs. Also higher chance of
+        reverting if some of the specified IDs leave the pool before the swap goes through.
+        @dev Overrides base swapTokenForSpecificNFTs to optimistically write to oracle prior to calcs and swap.
+        @param nftIds The list of IDs of the NFTs to purchase
+        @param maxExpectedTokenInput The maximum acceptable cost from the sender. If the actual
+        amount is greater than this value, the transaction will be reverted.
+        @param nftRecipient The recipient of the NFTs
+        @param isRouter True if calling from LSSVMRouter, false otherwise. Not used for
+        ETH pairs.
+        @param routerCaller If isRouter is true, ERC20 tokens will be transferred from this address. Not used for
+        ETH pairs.
+        @return inputAmount The amount of token used for purchase
+     */
+    function swapTokenForSpecificNFTs(
+        uint256[] calldata nftIds,
+        uint256 maxExpectedTokenInput,
+        address nftRecipient,
+        bool isRouter,
+        address routerCaller
+    )
+        external
+        payable
+        virtual
+        override
+        nonReentrant
+        returns (uint256 inputAmount)
+    {
+        // Store locally to remove extra calls
+        ILSSVMPairFactoryLike _factory = factory();
+        ICurve _bondingCurve = bondingCurve();
+
+        // Input validation
+        {
+            PoolType _poolType = poolType();
+            require(
+                _poolType == PoolType.NFT || _poolType == PoolType.TRADE,
+                "Wrong Pool type"
+            );
+            require((nftIds.length > 0), "Must ask for > 0 NFTs");
+        }
+
+        // TODO: optimistically write last price to historical observations
+        _writeObservationToOracle();
+
+        // Call bonding curve for pricing information
+        uint256 protocolFee;
+        (protocolFee, inputAmount) = _calculateBuyInfoAndUpdatePoolParams(
+            nftIds.length,
+            maxExpectedTokenInput,
+            _bondingCurve,
+            _factory
+        );
+
+        _pullTokenInputAndPayProtocolFee(
+            inputAmount,
+            isRouter,
+            routerCaller,
+            _factory,
+            protocolFee
+        );
+
+        _sendSpecificNFTsToRecipient(nft(), nftRecipient, nftIds);
+
+        _refundTokenToSender(inputAmount);
+
+        emit SwapNFTOutPair();
+    }
+
+    /**
+        @notice Sends a set of NFTs to the pair in exchange for token
+        @dev To compute the amount of token to that will be received, call bondingCurve.getSellInfo.
+        @dev Overrides base swapNFTForToken to optimistically write to oracle prior to calcs and swap.
+        @param nftIds The list of IDs of the NFTs to sell to the pair
+        @param minExpectedTokenOutput The minimum acceptable token received by the sender. If the actual
+        amount is less than this value, the transaction will be reverted.
+        @param tokenRecipient The recipient of the token output
+        @param isRouter True if calling from LSSVMRouter, false otherwise. Not used for
+        ETH pairs.
+        @param routerCaller If isRouter is true, ERC20 tokens will be transferred from this address. Not used for
+        ETH pairs.
+        @return outputAmount The amount of token received
+     */
+    function swapNFTsForToken(
+        uint256[] calldata nftIds,
+        uint256 minExpectedTokenOutput,
+        address payable tokenRecipient,
+        bool isRouter,
+        address routerCaller
+    ) external virtual override nonReentrant returns (uint256 outputAmount) {
+        // Store locally to remove extra calls
+        ILSSVMPairFactoryLike _factory = factory();
+        ICurve _bondingCurve = bondingCurve();
+
+        // Input validation
+        {
+            PoolType _poolType = poolType();
+            require(
+                _poolType == PoolType.TOKEN || _poolType == PoolType.TRADE,
+                "Wrong Pool type"
+            );
+            require(nftIds.length > 0, "Must ask for > 0 NFTs");
+        }
+
+        // TODO: optimistically write last price to historical observations
+        _writeObservationToOracle();
+
+        // Call bonding curve for pricing information
+        uint256 protocolFee;
+        (protocolFee, outputAmount) = _calculateSellInfoAndUpdatePoolParams(
+            nftIds.length,
+            minExpectedTokenOutput,
+            _bondingCurve,
+            _factory
+        );
+
+        _sendTokenOutput(tokenRecipient, outputAmount);
+
+        _payProtocolFeeFromPair(_factory, protocolFee);
+
+        _takeNFTsFromSender(nft(), nftIds, _factory, isRouter, routerCaller);
+
+        emit SwapNFTInPair();
+    }
+
+    /**
+     * Internal functions
+     */
+
+    /// @notice Writes an observation of the last price to the oracle prior to swap execution.
+    function _writeObservationToOracle() internal {
+        // TODO: implement ...
+        uint128 currentSpotPrice = spotPrice;
+    }
+}
